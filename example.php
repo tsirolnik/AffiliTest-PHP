51fa1ae036091adce7784b0dddfc143d9ad46f3f
Yehonatan Tsirolnik <y@tsirolnik.me>
2017-05-10 18:52:51 +0300

    compareToPreview added

--- example.php
+++ example.php
@@ -7,3 +7,12 @@
   var_dump($api->login('<email>', '<password>'));
 
   var_dump($api->test('http://cnn.com', 'us', AffiliTest\Devices::ANDROID));
+
+  // The preview can be a playstore/appstore URL or the package/app id
+  $previewURL = 'com.whatsapp';
+  var_dump($api->compareToPreview(
+    'https://play.google.com/store/apps/details?id=com.whatsapp',
+     'us',
+      AffiliTest\Devices::ANDROID,
+       $previewURL)
+       );
